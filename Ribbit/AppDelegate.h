ebfdc294e141eacb88631c1734502d50ccea2666
Chloe Horgan <super.chloe@gmail.com>
2015-12-17 14:51:45 -0500

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Chloe on 2015-12-17.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Chloe on 2015-12-17.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
