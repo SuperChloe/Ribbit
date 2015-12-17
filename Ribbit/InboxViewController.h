80baab52fb2ce358c42445b64f8b0439d69accfd
Chloe Horgan <super.chloe@gmail.com>
2015-12-17 16:41:18 -0500

    Added login/signup screens

---
+++ Ribbit/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by Chloe on 2015-12-17.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
