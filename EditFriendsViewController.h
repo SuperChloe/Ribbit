8312373caf27032fae4a4b7a867366790c409fc1
Chloe Horgan <super.chloe@gmail.com>
2015-12-20 16:36:43 -0500

    Added EditFriendsViewController

---
+++ EditFriendsViewController.h
@@ -0,0 +1,15 @@
+//
+//  EditFriendsViewController.h
+//  Ribbit
+//
+//  Created by Chloe on 2015-12-20.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *allUsers;
+
+@end
---
+++ EditFriendsViewController.m
@@ -0,0 +1,70 @@
+//
+//  EditFriendsViewController.m
+//  Ribbit
+//
+//  Created by Chloe on 2015-12-20.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import "EditFriendsViewController.h"
+#import <Parse/Parse.h>
+
+@interface EditFriendsViewController ()
+
+@end
+
+@implementation EditFriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        } else {
+            self.allUsers = objects;
+            [self.tableView reloadData];
+        }
+    }];
+    
+
+}
+
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.allUsers.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    cell.textLabel.text = user.username;
+    
+    return cell;
+}
+
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
